0d789298b42fee71d0dd99273d43d1795ce1429d
VictorDaguis <vthugo3321@gmail.com>
2026-05-03 17:22:05 -0300

    feat: CRUD de categorias

---
+++ src/models/Categoria.js
@@ -0,0 +1,8 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Categoria = sequelize.define('Categoria', {
+  nome: { type: DataTypes.STRING, allowNull: false, unique: true }
+});
+
+module.exports = Categoria;
--- src/models/index.js
+++ src/models/index.js
@@ -7,6 +7,9 @@ const Movimentacao = require('./Movimentacao');
 Reagente.hasMany(Movimentacao, { foreignKey: 'reagenteId', as: 'movimentacoes' });
 Movimentacao.belongsTo(Reagente, { foreignKey: 'reagenteId', as: 'reagente' });
 
+Categoria.hasMany(Reagente, { foreignKey: 'categoriaId' });
+Reagente.belongsTo(Categoria, { foreignKey: 'categoriaId' });
+
 Usuario.hasMany(Movimentacao, { foreignKey: 'usuarioId', as: 'movimentacoes' });
 Movimentacao.belongsTo(Usuario, { foreignKey: 'usuarioId', as: 'usuario' });
 
